dd49d2a2f249064181dad2b39fd128208a10d884
Lucas Pratas Oliveira <lpratas@proton.me>
2023-11-09 22:08:46 +0000

    add marker for current user location

--- nova_maps/lib/Views/HomePage.dart
+++ nova_maps/lib/Views/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -43,24 +44,15 @@ class _MyHomePageState extends State<MyHomePage> {
           
           FlutterMap(
             options: MapOptions(
-              initialCenter: LatLng(38.66242134778433, -9.205404686249338),
-              initialZoom: 16,
+              initialCenter: LatLng(38.66168, -9.20592),
+              initialZoom: 17,
             ),
             children: [
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.example.app',
               ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: LatLng(38.66242134778433, -9.205404686249338),
-                    width: 40,
-                    height: 40,
-                    child: FlutterLogo(),
-                  ),
-                ],
-              ),
+              CurrentLocationLayer(),
               RichAttributionWidget(
                 attributions: [
                   TextSourceAttribution(
