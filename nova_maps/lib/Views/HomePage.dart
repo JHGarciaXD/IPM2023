5d5cb87398f849676e8eeeec361fa38e0d6f5fe8
Pedro Cavaleiro <57799673+Plasmified@users.noreply.github.com>
2023-11-13 16:45:41 +0000

    Update HomePage.dart
    Added proper navigation.

--- nova_maps/lib/Views/HomePage.dart
+++ nova_maps/lib/Views/HomePage.dart
@@ -16,7 +16,23 @@ import 'OrsAPI.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+
+List listOfPoints = [];
+List<LatLng> points = [];
+
 class MyHomePage extends StatefulWidget {
+
+  Future<void> getCoordinates(String destination) async {
+    var response = await http.get(getRouteUrl("-9.20592,38.66168", destination));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      listOfPoints = data['features'][0]['geometry']['coordinates'];
+      points = listOfPoints
+          .map((p) => LatLng(p[1].toDouble(), p[0].toDouble()))
+          .toList();
+    }
+  }
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -26,6 +42,9 @@ class _MyHomePageState extends State<MyHomePage> {
   bool? notificationsEnabled = false;
   double squareOpacity = 0.5;
 
+  late LatLng markerCoordinates;
+  bool showContainer = false;
+
   // Navigation System
   List<String> yourNotificationList = [
     'Notification 1',
@@ -33,24 +52,31 @@ class _MyHomePageState extends State<MyHomePage> {
     'Notification 3',
     // Add more notifications as needed
   ];
-  List listOfPoints = [];
 
-  // Conversion of listOfPoints into LatLng(Latitude, Longitude) list of points
-  List<LatLng> points = [];
-
-  // Method to consume the OpenRouteService API
-  getCoordinates() async {
-    // Requesting for openrouteservice api
-    var response =
-        await http.get(getRouteUrl("-9.20592,38.66168", '-9.20575,38.66085'));
+  void test() {
     setState(() {
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body);
-        listOfPoints = data['features'][0]['geometry']['coordinates'];
-        points = listOfPoints
-            .map((p) => LatLng(p[1].toDouble(), p[0].toDouble()))
-            .toList();
-      }
+      points = listOfPoints
+          .map((p) => LatLng(p[1].toDouble(), p[0].toDouble()))
+          .toList();
+    });
+  }
+
+  void getCoordinates(String destination) async {
+    var response = await http.get(getRouteUrl("-9.20592,38.66168", destination));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      listOfPoints = data['features'][0]['geometry']['coordinates'];
+      points = listOfPoints
+          .map((p) => LatLng(p[1].toDouble(), p[0].toDouble()))
+          .toList();
+      test();
+    }
+  }
+
+  void showRectanglePopup(BuildContext context, LatLng point) {
+    setState(() {
+      showContainer = true;
+      markerCoordinates = point;
     });
   }
 
@@ -77,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Stack(
         children: [
+
           FlutterMap(
             options: MapOptions(
               initialCenter: LatLng(38.66168, -9.20592),
@@ -99,12 +126,96 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               MarkerLayer(
                 markers: [
-                  buildingButton(
-                      const LatLng(38.66115, -9.20345), "Building 2", context),
-                  restaurantButton(
-                      const LatLng(38.66175, -9.20465), "Cafeteria", context),
-                  buildingButton(
-                      const LatLng(38.66085, -9.20575), "Building 7", context),
+                  Marker(
+                      point: LatLng(38.66115, -9.20345),
+                      width: 100,
+                      height: 100,
+                      child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            IconButton(
+                              iconSize: 60,
+                              icon: const Icon(Icons.location_pin),
+                              color: Colors.blue.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66115, -9.20345));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                              iconSize: 20,
+                              icon: const Icon(Icons.circle),
+                              color: Colors.blue.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66115, -9.20345));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 14),
+                              iconSize: 22,
+                              icon: Icon(Icons.business),
+                              color: Colors.black,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66115, -9.20345));},
+                            ),
+                          ]
+                      )
+                  ),
+                  Marker(
+                      point: LatLng(38.66175, -9.20465),
+                      width: 100,
+                      height: 100,
+                      child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            IconButton(
+                              iconSize: 60,
+                              icon: const Icon(Icons.location_pin),
+                              color: Colors.orange.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66175, -9.20465));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                              iconSize: 20,
+                              icon: const Icon(Icons.circle),
+                              color: Colors.orange.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66175, -9.20465));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 14),
+                              iconSize: 22,
+                              icon: Icon(Icons.restaurant),
+                              color: Colors.black,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66175, -9.20465));},
+                            ),
+                          ]
+                      )
+                  ),
+                  Marker(
+                      point: LatLng(38.66085, -9.20575),
+                      width: 100,
+                      height: 100,
+                      child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            IconButton(
+                              iconSize: 60,
+                              icon: const Icon(Icons.location_pin),
+                              color: Colors.blue.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66085, -9.20575));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                              iconSize: 20,
+                              icon: const Icon(Icons.circle),
+                              color: Colors.blue.shade400,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66085, -9.20575));},
+                            ),
+                            IconButton(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 14),
+                              iconSize: 22,
+                              icon: Icon(Icons.business),
+                              color: Colors.black,
+                              onPressed: () {showRectanglePopup(context, LatLng(38.66085, -9.20575));},
+                            ),
+                          ]
+                      )
+                  ),
                 ],
               ),
               PolylineLayer(
@@ -115,6 +226,61 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+          if (showContainer && markerCoordinates != null)
+            Center(
+              child: GestureDetector(
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  color: Colors.grey.withOpacity(1.0),
+                  child:
+                    Stack(
+                        children: [
+                          IconButton(
+                            iconSize: 25,
+                            icon: const Icon(Icons.close),
+                            alignment: Alignment.topLeft,
+                            color: Colors.black,
+                            onPressed: () {
+                              setState(() {showContainer = false;});
+                            },
+                          ),
+                          Center(
+                            child:
+                          Stack(
+                            children: [
+                              IconButton(
+                                iconSize: 45,
+                                icon: const Icon(Icons.circle),
+                                color: Colors.blue,
+                                onPressed: () {
+                                  getCoordinates(markerCoordinates.longitude.toString()+","+markerCoordinates.latitude.toString());
+                                  setState(() {showContainer = false;});
+                                  // Action upon circle icon press
+                                },
+                              ),
+                              Positioned(
+                                top: 7,
+                                left: 7, // Adjust this value to align the second IconButton as needed
+                                child: IconButton(
+                                  iconSize: 30,
+                                  color: Colors.grey.shade100,
+                                  icon: const Icon(Icons.directions),
+                                  onPressed: () {
+                                    getCoordinates(markerCoordinates.longitude.toString()+","+markerCoordinates.latitude.toString());
+                                    setState(() {showContainer = false;});
+                                    // Action upon directions icon press
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          ),
+                    ]
+                ),
+                ),
+              ),
+            ),
           Positioned(
             top: 30,
             left: 30,
@@ -219,7 +385,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blueAccent,
-        onPressed: () => getCoordinates(),
+        onPressed: () {
+          test();
+          },
         child: const Icon(
           Icons.route,
           color: Colors.white,
